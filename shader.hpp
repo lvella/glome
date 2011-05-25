b5b371b145b810eb5cdf99e6ba163d8b043d6df9
Rodrigo Queiroz Saramago <deepmarolaest@gmail.com>
2011-05-25 04:25:28 -0300

    Removing old files

--- shader.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <GL/glew.h>
-
-GLuint setup_vshader(unsigned char *vcode, GLint len);
-GLuint setup_vfshader(unsigned char *vcode, unsigned char *fcode, GLint vlen, GLint flen);
--- world_proj.glsl
+++
@@ -1,9 +0,0 @@
-void main()
-{
-  vec4 tmp = gl_ModelViewMatrix * gl_Vertex;
-  tmp.xyz = tmp.xyz / (1.0 - tmp.w);
-  tmp.w = 1.0;
-  gl_Position = gl_ProjectionMatrix * tmp;
-  gl_FrontColor = gl_Color;
-  gl_FogFragCoord = length(gl_Position);
-}
