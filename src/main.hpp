e1d31375e135838919d8f4fc6302d1697abf396a
fynn <enriquefynn@gmail.com>
2011-06-21 00:00:21 +0300

    Created a game.cpp/hpp, not sure if is the best way... =P

--- src/main.hpp
+++ src/main.hpp
@@ -1,6 +1,20 @@
 #pragma once
 
+#include <boost/asio.hpp>
+#include <iostream>
+
+#include "world.hpp"
+
 extern const int WIDTH;
 extern const int HEIGHT;
 
 extern const float FOV;
+
+extern boost::asio::io_service gIOService;
+extern bool isServer;
+extern bool isClient;
+extern short port;
+extern World* world;
+extern bool isSplit;
+extern std::string host;
+
