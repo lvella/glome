a72b0c78a963c94d2b15f0a0a67326361d2f63dc
Lucas Clemente Vella <lvella@gmail.com>
2011-11-16 09:38:05 -0200

    Better fog parameter.

--- src/common/world.fragment.glsl
+++ src/common/world.fragment.glsl
@@ -7,7 +7,7 @@ void main()
 	const vec4 FOG_COLOR = vec4(0.0, 0.0, 0.0, 1.0);
 
 	// Exponential curve constant, based on the fixed pipeline fog
-	const float FOG_FACTOR = -1.15; // aprox. the same as -(0.8 / log(2))
+	const float FOG_FACTOR = -1.20; // aprox. the same as -(0.83 / log(2))
 
 	float f = exp2(FOG_FACTOR * fog_coord);
 	gl_FragColor = mix(FOG_COLOR, color, f);
