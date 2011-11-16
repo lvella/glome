21f292b82984bcde972c13b47b65d3f9ac98c5d4
Leonardo Alt <leonardoaltt@gmail.com>
2011-11-16 02:07:47 -0200

    pra compilar

--- src/common/protocol.hpp
+++ src/common/protocol.hpp
@@ -5,6 +5,27 @@
 #include <memory>
 #include <string>
 
+// Isso fica temporariamente..
+#define QUIT 0
+#define TURN 1 
+#define MOVE_F 2
+#define MOVE_B 3
+#define SHOT 4
+#define UP 5
+#define DOWN 6
+#define LEFT 7
+#define RIGHT 8
+#define SPIN_L 9
+#define SPIN_R 10
+#define COMMAND_BEGIN 0
+#define COMMAND_END 1
+#define JOIN_GAME 11
+#define NEW_SHIP 12
+#define INIT_POS 13
+#define UPDATE_SHIP 14
+#define SHIP_POS 15
+//
+
 #define PROTOCOL_SIZE sizeof(protocol)
 #define MAX_NAME_LEN 16 //get from another place
 
