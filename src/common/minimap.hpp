895f0e98281db3a7e5552e2debbcf7d872082c36
fynn <enriquefynn@gmail.com>
2011-11-29 18:00:35 -0200

    tabs and makecircle in other file

--- src/common/minimap.hpp
+++ src/common/minimap.hpp
@@ -2,10 +2,16 @@
 
 #include "object.hpp"
 
+
 class World;
 
 namespace MiniMap
 {
+	/*
+ * @param wstart Start width of minimap
+ * @param world Use to draw minimap in world
+ * @param center Center of minimap 
+ */
   void draw(int wstart, World* world, const Matrix4& center);
   void draw_dot(const Object &obj);
   void update();
