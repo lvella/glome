6c9df3809ca83180a78f3b27c492262cc92c386b
Lucas Clemente Vella <lvella@gmail.com>
2012-10-26 14:32:44 -0200

    Fixing indentation.

--- src/common/ship.cpp
+++ src/common/ship.cpp
@@ -90,33 +90,33 @@ Ship::load_guns(Mesh::Types type)
 void 
 Ship::load_engines(Mesh::Types type)
 {
-  int ret;
-  FILE *fd;
+	int ret;
+	FILE *fd;
 
-  const char* name = mesh_filename[int(type)];
-  // Load .gun file
-  {
+	const char* name = mesh_filename[int(type)];
+	// Load .gun file
+	{
 		unsigned int engine_pos;
-    std::stringstream dir;
-    dir << DATA_DIR << "/models/" << name << ".wire";
-    fd = fopen(dir.str().c_str(), "rb");
-    /* Read header of file */
+		std::stringstream dir;
+		dir << DATA_DIR << "/models/" << name << ".wire";
+		fd = fopen(dir.str().c_str(), "rb");
+		/* Read header of file */
 		fseek(fd, 8, SEEK_SET);
 		fread(&engine_pos, sizeof(unsigned int), 1, fd);
 		/* Poiter file to engine position */
-    fseek(fd, engine_pos, SEEK_SET);
-    assert(fd != NULL);
-  }
+		fseek(fd, engine_pos, SEEK_SET);
+		assert(fd != NULL);
+	}
 
-  {
-    // Reading Engine Matrix
-    ret = fread(&nengines, sizeof(nengines), 1, fd);
-    assert(ret == 1);
+	{
+		// Reading Engine Matrix
+		ret = fread(&nengines, sizeof(nengines), 1, fd);
+		assert(ret == 1);
 
-    ret = fread(&velocity[0][0], sizeof(float), 16, fd);
-    assert (ret == 16);
-  }
-  fclose(fd);
+		ret = fread(&velocity[0][0], sizeof(float), 16, fd);
+		assert (ret == 16);
+	}
+	fclose(fd);
 }
 
 void 
