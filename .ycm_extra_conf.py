c178e5fd756dbcb40a347b14854c3ed6f0397900
Lucas Clemente Vella <lvella@gmail.com>
2015-09-02 00:12:17 -0300

    Making YouCompleteMe work.

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,12 +1,23 @@
 import subprocess
 import re
 import sys
+import os.path
 
 def FlagsForFile( filename, **kwargs ):
-    make_run = subprocess.Popen(['make', 'flags'], stdout=subprocess.PIPE)
+    # find directory of the file
+    dirname = os.path.dirname(filename)
+
+    os.chdir(dirname)
+    make_run = subprocess.Popen(['make', '-s', 'flags'], stdout=subprocess.PIPE)
     flags = make_run.stdout.readline()
     make_run.kill()
-    flags = re.split(' +', flags.replace('\n', ''))
+    
+    def expand_path(x):
+        if x.startswith(('-I', '-L')):
+            return x[:2] + os.path.abspath(x[2:])
+        return x
+
+    flags = map(expand_path, re.split(' +', flags.replace('\n', '')))
     flags.extend(['-x', 'c++'])
     return {
         'flags': flags,
