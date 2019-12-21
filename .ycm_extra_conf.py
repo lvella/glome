4eee39e0d47fd82e6353f66d5d9cb0dae8381a23
Lucas Clemente Vella <lvella@gmail.com>
2019-12-21 18:41:47 +0000

    Updating to work with newer dev tools.
    Namely: YouCompleteMe and automake

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -3,26 +3,25 @@ import re
 import sys
 import os.path
 
-def FlagsForFile( filename, **kwargs ):
+def Settings(**kwargs):
     # find directory of the file
-    dirname = os.path.dirname(filename)
+    dirname = os.path.dirname(kwargs.get('filename', './'))
 
     os.chdir(dirname)
     make_run = subprocess.Popen(['make', '-s', 'flags'], stdout=subprocess.PIPE)
     flags = make_run.stdout.readline()
     make_run.kill()
-    
+
     def expand_path(x):
         if x.startswith(('-I', '-L')):
             return x[:2] + os.path.abspath(x[2:])
         return x
 
-    flags = map(expand_path, re.split(' +', flags.replace('\n', '')))
+    flags = [expand_path(f) for f in re.split(' +', flags.decode('utf-8').replace('\n', ''))]
     flags.extend(['-x', 'c++'])
     return {
-        'flags': flags,
-        'do_cache': True
+        'flags': flags
     }
 
 if __name__ == '__main__':
-   print(FlagsForFile(sys.argv[1]))
+    print(Settings(filename=sys.argv[1]))
