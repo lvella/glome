f3156655b9a784743165538e8ce94a4708544792
Lucas Clemente Vella <lvella@gmail.com>
2020-10-07 21:23:50 +0100

    Whitespace formatting.

--- src/common/options.cpp
+++ src/common/options.cpp
@@ -66,18 +66,18 @@ int parse_args(int argc, char** argv)
 	static struct option long_options[] =
 	{
 #ifndef NDEBUG
-			{"bot-count",   required_argument,	0,	'b'},
-			{"bot-screen",  no_argument,		0,	'v'},
+			{"bot-count",	required_argument,	0,	'b'},
+			{"bot-screen",	no_argument,		0,	'v'},
 #endif
 			{"full-screen",	no_argument,		0,	'f'},
-			{"width",		required_argument,	0,	'x'},
-			{"height",		required_argument,	0,	'y'},
+			{"width",	required_argument,	0,	'x'},
+			{"height",	required_argument,	0,	'y'},
 			{"is-server",	no_argument,		0,	's'},
 			{"is-client",	no_argument,		0,	'c'},
-			{"host",		required_argument,	0,	'h'},
-			{"port",		required_argument,	0,	'p'},
+			{"host",	required_argument,	0,	'h'},
+			{"port",	required_argument,	0,	'p'},
 			{"no-split",	no_argument,		0,	'l'},
-			{"vr",			no_argument, 		0,	'r'},
+			{"vr",		no_argument,		0,	'r'},
 			{0, 0, 0, 0}
 	};
 
